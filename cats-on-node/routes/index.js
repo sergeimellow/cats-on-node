b4ceae1493c903126cbafa986663c4a39f8e5193
arielayb <cyberspace009@gmail.com>
2015-12-09 00:46:04 -0500

    Made small progress on loading the image

--- cats-on-node/routes/index.js
+++ cats-on-node/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var google = require('googleapis');
+
+var google2 = "213";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cat MEME Generator' });
+  res.render('index', { title: 'Cat MEME Generator', google: google2 });
 });
 
 module.exports = router;
