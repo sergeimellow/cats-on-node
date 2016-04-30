f5b88bb09f79b42df6ef62cabde03ba26240821f
Sergei Mellow <sergei.mellow@gmail.com>
2016-04-30 01:10:32 -0400

    scratch code for getting mongoose talking with mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Mongoose import
+var mongoose = require('mongoose');
+// Mongoose connection to MongoDB (ted/ted is readonly)
+mongoose.connect('localhost:27017', function (error) {
+    if (error) {
+        console.log(error);
+    }
 });
+
+// Mongoose Schema definition
+var Schema = mongoose.Schema;
+var LinkSchema = new Schema({
+    link: String,
+});
+// Mongoose Model definition
+var Link = mongoose.model('links', LinkSchema);
+/* GET users listing. */
+var silence = new Link({ link: 'https://s-media-cache-ak0.pinimg.com/600x315/76/82/a7/7682a7ce87c79934bddaf9ad289e9d07.jpg' });
+
+silence.save(function (err, silence) {
+  if (err) return console.error(err);
+  //silence.speak();
+});
+
+router.get('/', function(req, res, next) {
+  Link.find({ link: "https://s-media-cache-ak0.pinimg.com/600x315/76/82/a7/7682a7ce87c79934bddaf9ad289e9d07.jpg" }, function (err, docs) {
+            res.json(docs);
+        });
+});
+
+
 
 module.exports = router;
